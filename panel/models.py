232c0bbfe292a3695db9a60773c8d0a0f3000d71
Michael <meijboom.am@gmail.com>
2021-01-16 14:46:36 +0100

    Add server model

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Server(models.Model):
+    host = models.CharField(max_length=40)
+    port = models.IntegerField(default=25565, blank=True)
+    password = models.CharField(max_length=255)
